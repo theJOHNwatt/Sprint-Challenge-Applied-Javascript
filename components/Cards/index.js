e5cf3ed6081d6d6f054c8378ab214691c7e32057
theJOHNwatt <j.watt10@gmail.com>
2019-09-06 11:19:56 -0700

    cards js coded, still need to work on getting info to show from the api

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,44 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardsContainer = document.querySelector('.cards-container');
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response);
+    const articlesData = newArticle(response.data);
+    cardsContainer.appendChild(articlesData)
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+function newArticle (articles) {
+    const
+        newCard = document.createElement('div'),
+        headline = document.createElement('div'),
+        author = document.createElement('div'),
+        imgContainer = document.createElement('div'),
+        authorImg = document.createElement('img'),
+        authorsName = document.createElement('span');
+
+    newCard.appendChild(headline);
+    newCard.appendChild(author);
+    author.appendChild(imgContainer);
+    author.appendChild(authorsName);
+    imgContainer.appendChild(authorImg);
+
+    newCard.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    headline.textContent = articles.headline;
+    authorImg.src = articles.authorPhoto; 
+    authorsName.textContent = `By ${articles.authorN}`;
+
+    return newCard;
+  }
+
+
